f4e195c8c89d9bb8605685e64a802d639ea5a7a7
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-03-25 21:45:30 +0100

    Lesson 2.1
    sin problema

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        //keep the player in bpunds
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+}
