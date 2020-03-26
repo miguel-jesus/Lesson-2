a45fabc5e9c30d2057498fb6aeb4d2fa3e5434e0
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-03-26 17:46:58 +0100

    Challenge 2
    la complicacion mas grande ha sido en los bonus en los que he tenido que buscar informacion sobre los invoke y he dejado comentado su funcionamiento y buscando informacion sobre el Time.time para hacer el segundo bonus

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogRate = 1; // Tiempo que el jugador tiene que esperar para disparar de nuevo
+    private float nextDog = 0; // tiempo desde el inicio después del cual el jugador puede disparar nuevamente
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextDog)
+        {
+            nextDog = Time.time + dogRate; // restablecer nextDog a la hora actual + dogRate
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int index = Random.Range(0, ballPrefabs.Length);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[index], spawnPos, ballPrefabs[index].transform.rotation);
+        Invoke("SpawnRandomBall", Random.Range(3, 5));
+        /*
+         Invocar solo llama a un método con retraso, invoca llamadas repetidas a un método con retraso y repite esa llamada de método durante el tiempo,
+         por lo que si en lugar de usar la repetición de invocación simplemente llama a invocar al final del método, 
+         comenzará a llamarlo recursivamente (una y otra vez) pero podrá establecer un nuevo retraso en cada llamada
+         */
+    }
+
+}
