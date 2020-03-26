2ecf0eda5855a5ad95e4fbe8c2644dd7dec5ae6f
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-03-26 15:36:19 +0100

    Lesson 2.3
    sin problema facil de entender el nuevo script y comentado

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)){
+            animalIndex = Random.Range(0,animalPrefabs.Length);
+            //randomizamos el lugar del spawn dentro de los valores que sabemos que tiene nuestro juego
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+            //orden de la instanciacion : 1-objeto a instanciar de la array. 2- Posicion del spawn 3- el quaternation rotation
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
